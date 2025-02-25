14e2d1365faff7b0ebd6271cdcf7ecead4c802e7
Arnav Agarwal <120201540+ArnavAgarwal-Mr-AR@users.noreply.github.com>
2025-02-25 11:50:23 +0530

    Update socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,11 +1,8 @@
 import { io } from 'socket.io-client';
 import { getToken } from './auth'; // implement this to get token from storage
-
-const { protocol, hostname } = window.location;
-const port = 3001; // Your backend server port
-
-export const socket = io(`${protocol}//${hostname}:${port}`, {
+const backendUrl = import.meta.env.BACKEND_URL;
+export const socket = io(backendUrl, {
   auth: {
-    token: getToken()
-  }
+    token: getToken(),
+  },
 });
