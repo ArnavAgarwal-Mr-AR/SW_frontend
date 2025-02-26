971e43f1c6c5dd0f98ce2e02bf1e7abd8c3ccb62
Arnav Agarwal <120201540+ArnavAgarwal-Mr-AR@users.noreply.github.com>
2025-02-27 01:47:58 +0530

    add css to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import LandingPage from './pages/LandingPage';
 import { LoginForm } from './components/auth/LoginForm';
 import { RegisterForm } from './components/auth/RegisterForm';
 import App from './App';
-
+import "./index.css";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
