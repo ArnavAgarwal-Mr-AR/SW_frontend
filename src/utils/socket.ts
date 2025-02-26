6b2ec496a2f682883bf429c7bf2bf59210b36893
Arnav Agarwal <120201540+ArnavAgarwal-Mr-AR@users.noreply.github.com>
2025-02-26 19:27:34 +0530

    Update socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 import { getToken } from './auth'; // implement this to get token from storage
-const backendUrl = import.meta.env.BACKEND_URL;
+const backendUrl = import.meta.env.BACKEND_URL | 'https://backend-pdis.onrender.com';
 export const socket = io(backendUrl, {
   auth: {
-    token: getToken(),
+    token: getTokenFromLocalStorageOrWherever(),
   },
 });
